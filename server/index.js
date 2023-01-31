6140de62c488cbe63b7d27107148374c13c2ae9e
rize-ma <masaki030603@gmail.com>
2023-01-31 20:10:45 +0900

    mongodbに接続し、Userスキーマを作成

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const PORT = 5000;
+require("dotenv").config();
 app.use(express.json());
+
+
+try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(process.env.MONGODB_URL);
+    console.log("接続");
+} catch (error) {
+    console.log(error);
+}
 
 app.listen(PORT, () => {
     console.log("ローカルサーバー起動中");
