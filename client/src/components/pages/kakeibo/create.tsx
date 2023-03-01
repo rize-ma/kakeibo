66e323672cf8decc08d79993d0320bc51defa83e
rize-ma <masaki030603@gmail.com>
2023-03-02 01:25:45 +0900

    家計簿作成画面のuiとコードの微修正

--- client/src/components/pages/kakeibo/create.tsx
+++ client/src/components/pages/kakeibo/create.tsx
@@ -1,7 +1,8 @@
-import { Alert, Card, CardBody, Input, Tab, Tabs, TabsHeader, Textarea, Typography } from "@material-tailwind/react";
+import { Alert, Card, CardBody, Input, Textarea, Typography } from "@material-tailwind/react";
+import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
 import { SegmentedControl, Select } from "@mantine/core";
 import { ChangeEventHandler, useState } from "react";
-import { changeFormInput } from "../../../types";
+import { changeFormInput, changeInputType, inputType, isExpenses, toggleCategoryData, voidFunction } from "../../../types";
 import { Button } from "@material-tailwind/react";
 import { moneyValidation } from "../../../utils/validation";
 import kakeiboApi from "../../../api/kakeiboApi";
@@ -16,28 +17,62 @@ const Kakeibo = () => {
   const [serverErrText, setServerErrText] = useState<string>("");
   const [successText, setSuccessText] = useState<string>("");
 
+  const EXPENSES_DATA: Array<string> = ["食費","外食費","日用品","交通費","衣服","趣味","その他"];
+  const INCOME_DATA: Array<string> = ["給料", "その他"];
+  const INPUT_TYPE_DATA: inputType = [{label: "支出",value: "expenses"},{label: "収入",value: "income"}];
+
+  const toggleCategoryData: toggleCategoryData = () => {
+    if(inputType === "expenses") {
+      return EXPENSES_DATA;
+    } 
+    if(inputType === "income"){
+      return INCOME_DATA;
+    }
+    return [""];
+  };
+
+  const changeInputType: changeInputType = (value:string) => {
+    if(value === "expenses") {
+      setInputType("expenses");
+      setCategory("食費");
+    }
+    if(value === "income") {
+      setInputType("income");
+      setCategory("給料");
+    }
+  };
+
   const changemoney: changeFormInput = (e) => setMoney(e.target.value);
-  const toggleLabelColor = () => setToggleLabel(!toggleLabel);
+  const toggleLabelColor: voidFunction = () => setToggleLabel(!toggleLabel);
   const changeDescription: ChangeEventHandler<HTMLTextAreaElement> = (e) => setDescription(e.target.value);
 
-  const isExpenses = () => {
+  const isExpenses: isExpenses = () => {
     if (inputType === "expenses") {
       return true;
     }
     return false;
   };
-  const createKakeibo = async () => {
-    setMoneyErrText(moneyValidation(money));
-    if(moneyErrText) return ;
+
+  const resetReaponseText: voidFunction = () => {
+    setServerErrText("");
+    setSuccessText("");
+  };
+  
+  const createKakeibo: voidFunction = async () => {
+    const moneyValidationRes = moneyValidation(money);
+    if(moneyValidationRes !== "") {
+      setMoneyErrText(moneyValidationRes);
+      return
+    }; 
     const newMoney = Number(money);
     
     try {
       await kakeiboApi.create({category,description, money: newMoney, expenses:isExpenses(), income: !isExpenses()});
-      setInputType("expenses");
       setCategory("食費");
       setMoney("");
       setDescription("");
       setSuccessText("登録が完了しました");
+      setTimeout(resetReaponseText,5000);
     } catch {
       setServerErrText("サーバーでエラーが発生しました。");
     }
@@ -46,24 +81,40 @@ const Kakeibo = () => {
     <div>
       <Card className="w-96 mb-8 ">
                 <CardBody className="flex flex-col items-center gap-4">
-                <Alert className="p-2" color="teal" show={successText ? true : false}>{successText}</Alert>
-                <Alert className="p-2" color="red" show={serverErrText ? true : false}>{serverErrText}</Alert>
+                <Alert 
+                color="teal" 
+                animate={{
+                  mount: { scale: 1, y: 0  },
+                  unmount: { scale: 0, y: 25 },
+                }}
+                show={successText ? true : false}
+                dismissible={{
+                  onClose: () => setSuccessText(""),
+                }}
+                >
+                  {successText}
+                </Alert>
+                <Alert
+                color="red"  
+                animate={{
+                  mount: { y: 0 },
+                  unmount: { y: 100 },
+                  }} 
+                show={serverErrText ? true : false}
+                dismissible={{
+                  onClose: () => setServerErrText(""),
+                }}
+                >
+                  {serverErrText}
+                </Alert>
                   <Typography variant="h3">
                       入力
+                      <ModeEditOutlineIcon className="ml-2"/>
                   </Typography>
                   <SegmentedControl
-                  data = {[
-                    {
-                      label: "支出",
-                      value: "expenses"
-                    },
-                    {
-                      label: "収入",
-                      value: "income"
-                    }
-                  ]}
+                  data = {INPUT_TYPE_DATA}
                   value={inputType}
-                  onChange={setInputType}
+                  onChange={changeInputType}
                   size="lg"
                   radius={10}
                   transitionDuration={500}
@@ -72,7 +123,7 @@ const Kakeibo = () => {
                     <Alert className="p-2" color="red" show={moneyErrText ? true : false}>{moneyErrText}</Alert>
                     <Input label="金額" size="lg" color="orange" type="number" onChange={changemoney} value={money}/>
                     <Select 
-                    data={["食費","外食費","日用品","交通費","衣服","趣味","その他"]} 
+                    data={toggleCategoryData()} 
                     placeholder="カテゴリー" 
                     className="w-full" 
                     label="カテゴリー"
