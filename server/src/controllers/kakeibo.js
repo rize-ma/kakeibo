aa82c34cb47924f7e358431e325076c4b1af9ec2
rize-ma <masaki030603@gmail.com>
2023-03-12 22:55:49 +0900

    バックエンド側の家計簿を1つ取得するAPIを作成

--- server/src/controllers/kakeibo.js
+++ server/src/controllers/kakeibo.js
@@ -26,7 +26,18 @@ exports.dateSearch = async (req, res) => {
         const kakeiboData = await kakeibo.find({ user: req.user._id, date: kakeiboDate });
         res.status(201).json({ data: { kakeiboData } });
     } catch (err) {
-        res.status(500).json(err);
+        res.status(500).json({ data: err });
+    }
+};
+
+exports.getOne = async (req, res) => {
+    const { kakeiboId } = req.params;
+    try {
+        const kakeibo = await kakeibo.findOne({ user: req.user._id, _id: kakeiboId });
+        if (!kakeibo) return res.status(404).json("家計簿が存在しません");
+        res.status(200).json({ data: kakeibo });
+    } catch (err) {
+        res.status(500).json({ data: err });
     }
 };
 
