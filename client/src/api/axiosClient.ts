d9c19e3ac54c6eb07e5777cb0d90d67b18cab0cd
rize-ma <masaki030603@gmail.com>
2023-04-07 07:56:45 +0900

    BASE_URLをデプロイしたAPIのURLに変更

--- client/src/api/axiosClient.ts
+++ client/src/api/axiosClient.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+//const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 const getToken = () => localStorage.getItem("token")
 
 const axiosClient = axios.create({
